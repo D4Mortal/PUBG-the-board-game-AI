6d840bd8d3e8aba44849ae9b973b465c8ae828a9
unknown <chirag8336@gmail.com>
2018-04-25 15:10:32 +1000

    numpy repre

--- Board.py
+++ Board.py
@@ -1,6 +1,6 @@
 # Chirag Rao Sahib      : 836011
 # Daniel Hao            : 834496
-# Date                  : 
+# Date                  :
 # Python version        : 3.6.4
 
 ###############################################################################
@@ -17,43 +17,43 @@ import sys
 # and faster attribute access
 
 class board(object):
-    
+
     __slots__ = ('state', 'move')
-     
+
     def __init__(self, state, move):
         self.state = state
         self.move = move
-    
+
 ###############################################################################
-        
+
     # function that returns the new board object created from the specified move
     def newMakeMove(self, move):
         newState = copy.deepcopy(self.state)
-        
-        # make changes in the newState according to the moves specified 
-        
+
+        # make changes in the newState according to the moves specified
+
         newBoard = board(newState, move)
         return newBoard
-    
+
 ###############################################################################
-        
+
     # function that make moves on the current object, changes the current state,
     # does not create a new board
     def _makeMove(self, move):
-        
-        
-        # make changes in the self.state according to the moves specified 
-        
-        
-        return 
- ###############################################################################
-   
 
- 
+
+        # make changes in the self.state according to the moves specified
+
+
+        return
+ ###############################################################################
+
+
+
 gameState = []
 for i in range(8):
     gameState.append(input().split())
-    
+
 print("show", sys.getsizeof(board(gameState, ((0,0),(0,1)))))
 print(gameState )
 print("show", sys.getsizeof(gameState[0]))
