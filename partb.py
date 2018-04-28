a538535530ddd8ad1d6c32f98d2af5a2997f71d5
Daniel Hao <danielhao461997@gmail.com>
2018-04-28 18:45:02 +1000

    added board elimination

--- partb.py
+++ partb.py
@@ -16,7 +16,7 @@ MAX_NODES = 1e5  # max number of nodes to expand
 MAX_DEPTH = 5e2  # max depth of tree to explore
 DEADEND = 1e5  # heavy (bad) score for deadends (heuristic)
 BLACK_MULTIPLIER = 1e3  # heavy (bad) score for more black pieces (heuristic)
-
+MAP = {1:2, 2:1}
 
 MODS = {'R': (0, 1),  # how each direction modifies a position
         '2R': (0, 2),
@@ -46,15 +46,17 @@ class Player():
         self.state[7,0] = CORNER
         self.state[7,7] = CORNER
         
-        self.node = board(self.state, None)
+        
         
         if colour[0] == 'w':
           self.player_colour = WHITE
+          self.node = board(self.state, None, WHITE)
           self.opp_colour = BLACK
+          
         else:
           self.player_colour = BLACK
           self.opp_colour = WHITE
-
+          self.node = board(self.state, None, BLACK)
 
 
 
@@ -97,16 +99,16 @@ class Player():
         
 class board(object):
  
-    __slots__ = ('state', 'move', 'score')
+    __slots__ = ('state', 'move', 'score', 'colour')
  
-    def __init__(self, state, move):
+    def __init__(self, state, move, colour):
         self.state = state
         self.move = move
+        self.colour = colour
         self.score = self.calculateScore()
- 
 ###############################################################################
         
-     # function that returns the new board object created from the specified move
+    # function that returns a new board object created from the specified move
     def newMakeMove(self, action):
         newState = copy.deepcopy(self.state)
         
@@ -127,8 +129,8 @@ class board(object):
           self.put_piece(newState, action_tuple[0][0], action_tuple[0][1], UNOCC)
           self.put_piece(newState, action_tuple[1][0], action_tuple[1][1], colour)
  
-        
-        newBoard = board(newState, action)
+        self.eliminateBoard(newState, colour)
+        newBoard = board(newState, action, self.colour)
         return newBoard
  
 ###############################################################################
@@ -136,7 +138,59 @@ class board(object):
     def put_piece(self, state, row, col, piece):
       state[row, col] = piece
       
+###############################################################################
+      
+    def isEliminated(self, board, row, col, piece):
+        '''
+        check whether the given piece will be eliminated by the corner
+            and/or surrounding opponents
+        '''
+        if piece == WHITE:
+            flag = BLACK
+        if piece == BLACK:
+            flag = WHITE
 
+        if row == 0 or row == 7:
+            checkLeft = posCheck(board, row, col, 'L')
+            checkRight = posCheck(board, row, col, 'R')
+            if checkLeft == flag or checkLeft == CORNER:
+                if checkRight == flag or checkRight == CORNER:
+                    return True
+
+        elif col == 0 or col == 7:
+            checkUp = posCheck(board, row, col, 'U')
+            checkDown = posCheck(board, row, col, 'D')
+            if checkUp == flag or checkUp == CORNER:
+                if checkDown == flag or checkDown == CORNER:
+                    return True
+
+        else:
+            # generate positions to check
+            check = [posCheck(board,row,col,i) for i in ['L','R','U','D']]
+            if check[0] == flag or check[0] == CORNER:
+                if check[1] == flag or check[1] == CORNER:
+                    return True
+            if check[2] == flag or check[2] == CORNER:
+                if check[3] == flag or check[3] == CORNER:
+                    return True
+
+        return False
+
+###############################################################################
+
+    def eliminateBoard(self, state, colour):
+        '''
+        returns updated board after necessary eliminations
+        '''
+        mapping = {1: [BLACK, WHITE], 2: [WHITE, BLACK]}
+        for piece in mapping[colour]:
+            for row, line in enumerate(state):
+                for col, symbol in enumerate(line):
+                    if symbol == piece:
+                        if self.isEliminated(state, row, col, piece):
+                            state[row][col] = UNOCC
+        
+        return state
 ###############################################################################
       
     # function that make moves on the current object, changes the current state,
@@ -160,14 +214,23 @@ class board(object):
           self.put_piece(self.state, action_tuple[0][0], action_tuple[0][1], UNOCC)
           self.put_piece(self.state, action_tuple[1][0], action_tuple[1][1], colour)
  
-        
+        self.eliminateBoard(self.state, colour)
         self.move = action # update the move that brought it to this state
         return
     
 ###############################################################################
         
     def calculateScore(self):
-        return 4
+        blackCount = 0
+        whiteCount = 0
+        for row in self.state:
+            for symbol in row:
+                if symbol == BLACK:
+                    blackCount += 1
+                if symbol == WHITE:
+                    whiteCount += 1
+        if self.colour == WHITE: return whiteCount - blackCount
+        else: return blackCount - whiteCount
 ###############################################################################    
     
     def isComplete(self):
@@ -178,63 +241,72 @@ class board(object):
         return False
     
 ###############################################################################
-    def genChild(self):
+    def genChild(self, colour):
         
         row = 0
-        actions = defaultdict(list)
         action = []
         action_tuple = ()
         for r in self.state:
             col = 0
             for element in r:
-                if element == WHITE:
+                if element == colour:
 
                     if row + 1 < 8:
                         if posCheck(self.state, row, col, 'D') == UNOCC:
-                            actions[str(row) + str(col)].append(str(row + 1) + str(col))
-                            action_tuple = ((row, col), (row +1, col))
+
+                            action_tuple = ((row, col), (row + 1, col))
                             action.append(self.newMakeMove(action_tuple))
         
                         elif posCheck(self.state, row, col, 'D') == WHITE or posCheck(self.state, row, col, 'D') == BLACK:
                             if row + 2 < 8:
                                 if posCheck(self.state, row, col, '2D') == UNOCC:
-                                     actions[str(row) + str(col)].append(str(row + 2) + str(col))
-                                     action_tuple = ((row, col), (row +1, col))
+
+                                     action_tuple = ((row, col), (row + 2, col))
                                      action.append(self.newMakeMove(action_tuple))
+                                     
                     if row - 1 >= 0:
                         if posCheck(self.state, row, col, 'U') == UNOCC:
-                            actions[str(row) + str(col)].append(str(row - 1) + str(col))
 
+                            action_tuple = ((row, col), (row - 1, col))
+                            action.append(self.newMakeMove(action_tuple))
         
                         elif posCheck(self.state, row, col, 'U') == WHITE or posCheck(self.state, row, col, 'U') == BLACK:
                             if row - 2 >= 0:
                                 if posCheck(self.state, row, col, '2U') == UNOCC:
-                                    actions[str(row) + str(col)].append(str(row - 2) + str(col))
-            
+
+                                    action_tuple = ((row, col), (row - 2, col))
+                                    action.append(self.newMakeMove(action_tuple))
                     
                     
                     if col + 1 < 8:
                         if posCheck(self.state, row, col, 'R') == UNOCC:
-                            actions[str(row) + str(col)].append(str(row) + str(col + 1))
+
+                            action_tuple = ((row, col), (row, col + 1))
+                            action.append(self.newMakeMove(action_tuple))
         
                         elif posCheck(self.state, row, col, 'R') == WHITE or posCheck(self.state, row, col, 'R') == BLACK:
                             if col + 2 < 8:
                                 if posCheck(self.state, row, col, '2R') == UNOCC:
-                                    actions[str(row) + str(col)].append(str(row) + str(col + 2))
-                    
+
+                                    action_tuple = ((row, col), (row, col + 2))
+                                    action.append(self.newMakeMove(action_tuple))
+                                    
                     if col - 1 >= 0:
                         if posCheck(self.state, row, col, 'L') == UNOCC:
-                            actions[str(row) + str(col)].append(str(row) + str(col - 1))
+
+                            action_tuple = ((row, col), (row, col - 1))
+                            action.append(self.newMakeMove(action_tuple))
         
                         elif posCheck(self.state, row, col, 'L') == WHITE or posCheck(self.state, row, col, 'L') == BLACK:
                             if col - 2 >= 0:
                                 if posCheck(self.state, row, col, '2L') == UNOCC:
-                                    actions[str(row) + str(col)].append(str(row) + str(col - 2))
-    
+
+                                    action_tuple = ((row, col), (row, col - 2))
+                                    action.append(self.newMakeMove(action_tuple))
                 col += 1
             row += 1
             
-        return actions
+        return action
         
 
 def testMemUsage():
@@ -255,12 +327,19 @@ def testrun(me = 'WHITE'):
     # update board tests
     move = ((0,1), (3,4))
     move2 = ((3,4), (6,6))
+    move3 = ((3,5), (4,5))
+    move4 = ((6,6),(5,6))
     place = (6,5)
     null_move = None
 
     print('before update')
     game.put_piece(0, 1, BLACK)  # example for move
+    print()
+    game.put_piece(4, 7, WHITE)  # example for move
     game.put_piece(4, 4, WHITE)  # example for move
+    game.put_piece(4, 6, WHITE)  # example for move
+    game.put_piece(3, 5, BLACK)  # example for move
+    game.put_piece(3, 6, BLACK)  # example for move
     print(game.node.state)
 
     print('after update')
@@ -271,7 +350,21 @@ def testrun(me = 'WHITE'):
     game.update(move2)
     print(game.node.state)
     
+    print('after update 3')
+    game.update(move3)
+    print(game.node.state)
+    
+    print('after update 4')
+    game.update(move4)
+    print(game.node.state)
+    
     print(game.node.isComplete())
-    print(game.node.genChild())
+    
+#    for a in game.node.genChild(BLACK):
+#        print("this this generated")
+#        print(a.state)
+    
 testrun()
 #testMemUsage()
+selfColour = WHITE
+oppColour = BLACK
